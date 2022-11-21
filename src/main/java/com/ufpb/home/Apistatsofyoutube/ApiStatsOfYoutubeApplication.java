57efc7956a377b4296ac2c1f2021833d4fe9a756
Vinicius Teixeira <viniciusteixeiravini16@gmail.com>
2022-11-20 23:25:50 -0300

    commit initial

---
+++ src/main/java/com/ufpb/home/Apistatsofyoutube/ApiStatsOfYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.home.Apistatsofyoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStatsOfYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStatsOfYoutubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpb/home/Apistatsofyoutube/ApiStatsOfYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpb.home.Apistatsofyoutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStatsOfYoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
